c75f45a7843f0564ae20cfd6d3194892b51ad4e7
Mzuvukile Ntwana <Mzuvukile.Ntwana@mixtelematics.com>
2022-03-28 10:28:35 +0200

    first code check in for new repo

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using MiX.ResourceData.Client;
+
+
+string resourceDataUri = "http://resourcedata.int.development.domain.local";
+long assetId = 958802356208365568;
+
+await ResourceDataClient.RegisterRepositoryAsync(resourceDataUri).ConfigureAwait(false);
+
+var latestTripForAsset = (await ResourceDataClient.Trips.GetLatestAsync(new List<long>() { assetId }, 1).ConfigureAwait(false)).FirstOrDefault();
+
+if (DateTime.UtcNow.Subtract(latestTripForAsset.EndDateTime).TotalHours > 5)
+    Console.WriteLine("Trip is too old, there is a problem");
+
+Console.Read();
